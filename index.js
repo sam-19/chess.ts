b8f9f0ae97c42181704f56af27ccd60e6f536931
sam-19 <26563023+sam-19@users.noreply.github.com>
2023-10-07 18:41:46 +0300

    Export all classes from root as well
    Signed-off-by: sam-19 <26563023+sam-19@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-import Chess from './dist/chess'
+import Annotation from "./src/annotation"
+import Board from "./src/board"
+import Chess from "./src/chess"
+import Color from "./src/color"
+import Fen from "./src/fen"
+import Flags from "./src/flags"
+import Game from "./src/game"
+import ChessHeaders from "./src/headers"
+import Move from "./src/move"
+import Nag from "./src/nag"
+import Options from "./src/options"
+import Piece from "./src/piece"
+import ChessTimer from "./src/timers"
+import Turn from "./src/turn"
 
+export {
+    Annotation,
+    Board,
+    Chess,
+    ChessHeaders,
+    ChessTimer,
+    Color,
+    Fen,
+    Flags,
+    Game,
+    Move,
+    Nag,
+    Options,
+    Piece,
+    Turn,
+}
 export default Chess
